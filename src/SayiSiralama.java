2ed87fd2be903406e260d28bf4b8f3fb84662a10
egeyalcin <yalcinegehan@gmail.com>
2022-11-02 14:01:24 +0300

    sayi siralamasi

---
+++ src/SayiSiralama.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class SayiSiralama {
+
+    public static void main(String[] args) {
+
+        int a,b,c;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Please enter number 1: ");
+        a = keyboard.nextInt();
+
+        System.out.print("Please enter number 2: ");
+        b = keyboard.nextInt();
+
+        System.out.print("Please enter number 3: ");
+        c = keyboard.nextInt();
+        int temp;
+
+        if (a>b){
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        if (a>c){
+            temp = a;
+            a = c;
+            c = temp;
+        }
+        if (b>c){
+            temp = b;
+            b = c;
+            c = temp;
+        }
+
+        System.out.println(a + " " + b + " " + c );
+
+    }
+}
